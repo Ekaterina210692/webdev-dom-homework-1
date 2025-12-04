0752f5e24a64dca0b5a5254b004cc6ea607b09d2
Ekaterina210692 <ekaterinavasileva13@gmail.com>
2025-12-04 20:23:51 +0500

    1

--- modules/api.js
+++ modules/api.js
@@ -4,35 +4,39 @@ export const fetchComments = () => {
   return fetch(bazeUrl + "/comments")
     .then((response) => response.json())
     .then((responseData) => {
-        const appComments = responseData.comments.map((comment) => {
- return {
-        name: comment.author.name,
-        text: comment.text,
-        date: new Date(comment.date),
-        isLiked: false,
-        likesCount: comment.likes,
-        }
-      })
-      return appComments
-}
+      const appComments = responseData.comments.map((comment) => {
+        return {
+          name: comment.author.name,
+          text: comment.text,
+          date: new Date(comment.date),
+          isLiked: false,
+          likesCount: comment.likes,
+        };
+      });
+      return appComments;
+    });
+};
 
 export const postComment = async (comment, name) => {
   try {
     const dataToSend = {
       name: name,
-      text: comment
-    }
-    const response = await fetch("https://wedev-api.sky.pro/api/v1/ekaterinasin/comments", {
-      method: "POST",
-      body: JSON.stringify({
-        name: name,
-        text: comment,
-      }),
-    });
+      text: comment,
+    };
+    const response = await fetch(
+      "https://wedev-api.sky.pro/api/v1/ekaterinasin/comments",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          text: comment,
+        }),
+      }
+    );
 
-        if (!response.ok) {
-            throw new Error(`Ошибка: ${response.statusText}`);
-        }
+    if (!response.ok) {
+      throw new Error(`Ошибка: ${response.statusText}`);
+    }
 
     return response.json();
   } catch (error) {
